3ebfdbd5d15be98d682c9f6ebc78026cf99d2a46
Stanley Jin <jinwangjitai@outlook.com>
2020-08-09 14:47:24 +1000

    feature: update message display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,7 @@
 
 $(document).on("turbolinks:load", () => {
   $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
 });
